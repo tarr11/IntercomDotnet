5c64b013248b60642b9e0a030ea41734675f3eda
Mike Goodfellow <mdgoodfellow@gmail.com>
2015-06-19 14:36:43 +0100

    CHANGED: Clean up the IntercomClient class ADDED: .editorconfig file

--- IntercomDotnet/IntercomClient.cs
+++ IntercomDotnet/IntercomClient.cs
@@ -6,35 +6,29 @@ namespace IntercomDotNet
     {
         public const string ApiRoot = "https://api.intercom.io/";
 
-        public IntercomClient(string appid, string apikey, Users users, Notes notes, Tags tags, Events events)
+        public static IntercomClient GetClient(string appid, string apiKey)
         {
-            ApiKey = apikey;
-            AppId = appid;
+            var client = new Client
+                             {
+                                 UserName = appid,
+                                 Password = apiKey,
+                                 ApiRoot = ApiRoot
+                             };
+
+            return new IntercomClient(
+                new Users(client),
+                new Notes(client),
+                new Tags(client),
+                new Events(client));
+        }
+
+        private IntercomClient(Users users, Notes notes, Tags tags, Events events)
+        {
             Users = users;
             Notes = notes;
             Tags = tags;
             Events = events;
         }
-
-        public static IntercomClient GetClient(string appid, string apiKey)
-        {
-            var client = new Client
-                {
-                    UserName = appid,
-                    Password = apiKey,
-                    ApiRoot = ApiRoot
-                };
-
-            return new IntercomClient(appid, apiKey,
-                                      new Users(client),
-                                      new Notes(client),
-                                      new Tags(client),
-                                      new Events(client)
-                );
-        }
-
-        public string ApiKey { get; private set; }
-        public string AppId { get; private set; }
 
         public Users Users { get; private set; }
         public Notes Notes { get; private set; }
