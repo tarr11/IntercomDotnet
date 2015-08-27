0218ddcd44ac6d2ea6e3ddf2f70291d5418acf31
durop <ssd84@hotmail.com>
2015-08-27 14:36:39 -0300

    Added get methods

--- IntercomDotnet/Resources/Companies.cs
+++ IntercomDotnet/Resources/Companies.cs
@@ -21,5 +21,41 @@ namespace IntercomDotNet.Resources
                     request.AddBody(hash);
                 });
         }
+        
+        public dynamic Get(string name, string companyId) 
+        {
+            return Client.Execute(BaseUrl, Method.GET, request =>
+            {
+                if (name != null)
+                    request.AddParameter("name", name);
+
+                if (companyId != null)
+                    request.AddParameter("company_id", companyId);
+            });
+        }
+
+        public dynamic GetCompanyList(string tagId = null, string segmentId = null)
+        {
+            return Client.Execute(BaseUrl, Method.GET, request =>
+            {
+                if (tagId != null)
+                    request.AddParameter("tag_id", tagId);
+
+                if (segmentId != null)
+                    request.AddParameter("segment_id", segmentId);
+            });
+        }
+
+        public dynamic GetCompanyList(int page = 1, int perPage = 50, string order = null)
+        {
+            return Client.Execute(BaseUrl, Method.GET, request =>
+            {
+               request.AddParameter("page", page);
+               request.AddParameter("per_page", perPage);
+
+                if (order != null)
+                    request.AddParameter("order", order);
+            });
+        }
     }
 }
